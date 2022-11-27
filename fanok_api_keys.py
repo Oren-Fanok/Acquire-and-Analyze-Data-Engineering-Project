274b9d7402011b9c0772c7f9c63059e438e43c98
Oren Fanok <112598531+Oren-Fanok@users.noreply.github.com>
2022-11-27 13:56:17 -0700

    Add files via upload

---
+++ fanok_api_keys.py
@@ -0,0 +1,8 @@
+
+# From the "project set-up" experience
+api_key = 'Ih8ySfIK1lhHkTeJNOS9SYrJi'
+api_key_secret = '3gweSL7ZvM60pNLccyVGC5v2kCMB5grO6SQGJm57FKvmcQ45PX'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMtZggEAAAAAZpNQCUSStwThX6RsBHSSECGgofU%3Diy7ue8cJCJjb7xF1ExRcqZdJwLg7B4dpOwP5auRTNI9qpVNoat'
+access_token = '1565117397522325504-BZwDZhuBTQRUZUsodjDMjto5gqYY5R'
+access_token_secret = 'iU1Onn5dKGRBvxPqpmQzzWMx7sFaiTG5S55PBUupuosfs'
+
